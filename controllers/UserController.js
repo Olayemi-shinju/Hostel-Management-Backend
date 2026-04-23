d2a1366538483123c3b41bd9aa4e9bf11547bb5c
olayemi-shinju <awoyemiolayemi27@gmail.com>
2026-04-23 11:46:23 +0100

    change cookie life span to 24hrs

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -70,9 +70,9 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // IMPORTANT for localhost
+      secure: false,
       sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000
+      maxAge: 24 * 60 * 60 * 1000
     });
 
     res.status(200).json({
